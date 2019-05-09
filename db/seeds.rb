12c8561b34a76e8c5dd5ad50e1872eab4253aa36
Teigan H <teigan.hockman@gmail.com>
2019-05-09 19:49:30 -0400

    Add more plants to seed and styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,35 @@ Plant.create!(
     description: 'Pilea peperomioides, known as Chinese money plant, pancake plant, UFO plant, lefse plant, missionary plant or mirror grass, is a species of flowering plant in the nettle family Urticaceae, native to Yunnan and Sichuan provinces in southern China.',
     plant_image: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_pilea-peperomioides_mini-ezra-planter_plant-poke_1024x1024.jpg?v=1554842760'
 )
+Plant.create!(
+    scientific_name: 'Zantedeschia',
+    name: 'Calla Lilly',
+    description: 'Zantedeschia is a genus of 8 species of herbaceous, perennial, flowering plants in the family Araceae, native to southern Africa from South Africa north to Malawi. The genus has been introduced on all continents except Antarctica.',
+    plant_image: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_plant-calla-lily_prospect_blush_5_1024x1024.jpg?v=1557394006'
+)
+Plant.create!(
+    scientific_name: 'Hoya kerrii',
+    name: 'Hoya Heart',
+    description: 'Hoya kerrii is a species of Hoya native to the south-east of Asia. Its eponymous collector is Arthur Francis George Kerr, British physician and botanist. As the thick leaves are heart-shaped, the plant is sometimes named "Lucky-heart". In Europe, it is sold for Saint Valentines Day.',
+    plant_image: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_succulent_hoya-heart_ezra_sonora_800x.jpg?v=155726509'
+)
+Plant.create!(
+    scientific_name: 'Philodendron',
+    name: 'Philodendron',
+    description: 'Philodendron is a large genus of flowering plants in the Araceae family. As of September 2015, the World Checklist of Selected Plant Families accepted 489 species; other sources accept different numbers. Regardless of number of species, the genus is the second-largest member of the Araceae family.',
+    plant_image: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_philodendron-plant-green_grant_pale-red_5_800x.jpg?v=1548264640'
+)
+Plant.create!(
+    scientific_name: 'Monstera deliciosa',
+    name: 'Monstera',
+    description: 'Monstera deliciosa, the ceriman, is a species of flowering plant native to tropical forests of southern Mexico, south to Panama. It has been introduced to many tropical areas, and has become a mildly invasive species in Hawaii, Seychelles, Ascension Island and the Society Islands.',
+    plant_image: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_monstera-6_grant_mint_7_1500x.progressive.jpg?v=1557394356'
+)
+Plant.create!(
+    scientific_name: 'Calathea',
+    name: 'Calathea Medallion',
+    description: 'Calathea is a genus of plants belonging to the family Marantaceae. There are several dozen species in this genus. Native to the tropical Americas, many of the species are popular as pot plants due to their decorative leaves and, in some species, colorful inflorescences.',
+    plant_image: 'http://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_calathea-medallion_prospect_pale-grey-7_1024x1024.jpg?v=1553190924'
+)
+
+

ebfa04f4849e8f3fd215510ef27399bb72560f3f
Teigan H <teigan.hockman@gmail.com>
2019-05-09 18:02:36 -0400

    Updating for heroku

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -35,7 +35,7 @@ Knock.setup do |config|
   ## Configure the key used to sign tokens.
   ##
   ## Default:
-  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  config.token_secret_signature_key = -> { Rails.application.credentials.read }
   # config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
 
   ## If using Auth0, uncomment the line below
